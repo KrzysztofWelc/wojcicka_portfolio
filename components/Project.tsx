cfe97b7ffd4690eb886a248e33d293d605902e09
krzysztofwelc <krzysiekwelc18@gmail.com>
2024-10-21 21:35:17 +0200

    project component image zoom

--- components/Project.tsx
+++ components/Project.tsx
@@ -11,12 +11,14 @@ interface ProjectProps {
 export default function Project({ title, tags, description, image, imageAlt }: ProjectProps) {
     return (
         <figure>
-            <Image src={image} alt={imageAlt}
-                width="0"
-                height="0"
-                quality={100}
-                sizes="100%"
-                className="w-full" />
+            <div className="inline-block overflow-hidden rounded-3xl">
+                <Image src={image} alt={imageAlt}
+                    width="0"
+                    height="0"
+                    quality={100}
+                    sizes="100%"
+                    className="w-full hover:scale-110 duration-500" />
+            </div>
             <figcaption>
                 <h3 className='text-4xl mt-7'>{title}</h3>
                 <div className='text-2xl mt-5 text-secondary tracking-wider'>{tags}</div>
