66983c88e0d40c674a055c7969761b8c61caf9d9
krzysztofwelc <krzysiekwelc18@gmail.com>
2024-11-11 17:18:18 +0100

    responsive mood board

--- components/MoodBoard.tsx
+++ components/MoodBoard.tsx
@@ -4,22 +4,44 @@ import Image from 'next/image';
 
 export default function MoodBoard() {
   return (
-    <div className='flex gap-4'>
-      <div className='flex flex-col gap-4 flex-1 *:w-full'>
-        <Image src='/moodboard/2.png' width={300} height={150} alt='' />
-        <Image src='/moodboard/3.png' width={300} height={150} alt='' />
-        <Image src='/moodboard/9.png' width={300} height={150} alt='' />
-      </div>
-      <div className='flex flex-col gap-4 flex-1 *:w-full'>
-        <Image src='/moodboard/8.png' width={300} height={150} alt='' />
-        <Image src='/moodboard/4.png' width={300} height={150} alt='' />
-      </div>
-      <div className='flex flex-col gap-4 flex-1 *:w-full'>
-        <Image src='/moodboard/5.png' width={300} height={150} alt='' />
+    <>
 
-        <Image src='/moodboard/6.png' width={300} height={150} alt='' />
-        <Image src='/moodboard/7.png' width={300} height={150} alt='' />
+      <div className='hidden md:flex md:gap-4 gap-2'>
+        <div className='flex flex-col md:gap-4 gap-2 flex-1 *:w-full'>
+          <Image src='/moodboard/2.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/3.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/9.png' width={300} height={150} alt='' />
+        </div>
+        <div className='flex flex-col md:gap-4 gap-2 flex-1 *:w-full'>
+          <Image src='/moodboard/8.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/4.png' width={300} height={150} alt='' />
+        </div>
+        <div className='flex flex-col md:gap-4 gap-2 flex-1 *:w-full'>
+          <Image src='/moodboard/5.png' width={300} height={150} alt='' />
+
+          <Image src='/moodboard/6.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/7.png' width={300} height={150} alt='' />
+        </div>
       </div>
-    </div>
+
+      <div className='md:hidden flex md:gap-4 gap-2'>
+        <div className='flex flex-col md:gap-4 gap-2 flex-1 *:w-full'>
+          <Image src='/moodboard/8.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/2.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/3.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/9.png' width={300} height={150} alt='' />
+        </div>
+
+        <div className='flex flex-col md:gap-4 gap-2 flex-1 *:w-full'>
+          <Image src='/moodboard/5.png' width={300} height={150} alt='' />
+
+          <Image src='/moodboard/6.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/7.png' width={300} height={150} alt='' />
+          <Image src='/moodboard/4.png' width={300} height={150} alt='' />
+        </div>
+      </div>
+
+
+    </>
   );
 }
