ea01fac57da0afa137e2d6936da3756c907fdbb2
krzysztofwelc <krzysiekwelc18@gmail.com>
2024-11-25 19:34:46 +0100

    more

--- app/projects/esboarding/page.tsx
+++ app/projects/esboarding/page.tsx
@@ -1,3 +1,4 @@
+import Gallery from "@/components/Gallery"
 import Image from "next/image"
 
 export default function ESBoarding() {
@@ -155,8 +156,164 @@ export default function ESBoarding() {
                         <h3 className="h3">Persona developmemnt</h3>
                         <p>Based on the research and insights from students starting their studies in Reutlingen, two personas were created. These personas represent the key characteristics, goals, motivations, frustrations, and interests of 5th-semester IMX students who come to Reutlingen to continue their studies.</p>
 
+                        <Gallery>
+                            <Image
+                                src='/esb/personas/migel.png'
+                                width={870}
+                                height={1231}
+                                alt="persona card of Migel"
+                            />
+                            <Image
+                                src='/esb/personas/anna.png'
+                                width={870}
+                                height={1231}
+                                alt="persona card of Anna"
+                            />
+                        </Gallery>
+                        <div className="my-10">
+                            <h3 className="h3">Mission Statement</h3>
+                            <p className="mb-10">
+                                “To make the transition to a new educational environment as smooth and stress-free as possible, empowering students with the tools and information they need to succeed.”
+                            </p>
 
+                            <h3 className="h3">Attributes</h3>
+                            <p>Reliable, supportive, fun, international,  user-friendly</p>
+                        </div>
                     </div>
+                </section>
+                <h2 className="text-4xl mb-20">UX Design</h2>
+                <section className="mx-10">
+                    <div className="mb-10">
+                        <h3 className="h3">Prioritization</h3>
+                        <ul className='list-disc pl-9'>
+                            <li>Features that addressed immediate student concerns, such as orientation schedules, campus maps, and contact information for student services.</li>
+                            <li>Features that would engage students beyond the initial onboarding phase, such as social integration tools and personalized onboarding checklists.</li>
+                        </ul>
+                    </div>
+
+                    <div className="mb-10">
+                        <h3 className="h3">Feature roadmap</h3>
+                    </div>
+
+                    <div className="mb-10">
+                        <h3 className="h3">Information architecture</h3>
+                        <p>The information architecture of the ESBoarding app is designed for easy and intuitive access to essential information. The main categories include:</p>
+                        <ol className='list-disc pl-9 mb-4'>
+                            <li>Before the Journey</li>
+                            <li>In Reutlingen</li>
+                            <li>ESB Business School</li>
+                            <li>Language Learning</li>
+                        </ol>
+                        <p>Since users can start using the application at different times, it is possible to work on tasks from multiple phases at the same time.</p>
+                    </div>
+
+                    <Image
+                        src='/esb/chart.png'
+                        width={1051}
+                        height={784}
+                        alt="ESBoarding flow chart"
+                        className="mb-10"
+                    />
+
+                    <div className="mb-10">
+                        <h3 className="h3">Early wireframes</h3>
+                        <Gallery>
+                            <Image
+                                src='/esb/earlyWireframes/1.png'
+                                width={1087}
+                                height={753}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/earlyWireframes/2.png'
+                                width={1087}
+                                height={753}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/earlyWireframes/3.png'
+                                width={1087}
+                                height={753}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/earlyWireframes/4.png'
+                                width={1087}
+                                height={753}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/earlyWireframes/5.png'
+                                width={1087}
+                                height={753}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/earlyWireframes/6.png'
+                                width={1087}
+                                height={753}
+                                alt=""
+                            />
+                        </Gallery>
+                    </div>
+
+                </section>
+                <h2 className="text-4xl mb-20">UI Design</h2>
+                <section className="mb-10">
+                    <div className="mb-10">
+                        <h3 className="h3">UI Library Conponents</h3>
+                        <Image
+                            src='/esb/UIComponents.png'
+                            width={1050}
+                            height={568}
+                            alt=""
+                        />
+                    </div>
+                    <div className="mb-10">
+                        <h3 className="h3">Final Designs</h3>
+                        <Gallery>
+                            <Image
+                                src='/esb/finalDesigns/1.png'
+                                width={1068}
+                                height={800}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/finalDesigns/2.png'
+                                width={1068}
+                                height={800}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/finalDesigns/3.png'
+                                width={1068}
+                                height={800}
+                                alt=""
+                            />
+                            <Image
+                                src='/esb/finalDesigns/4.png'
+                                width={1068}
+                                height={800}
+                                alt=""
+                            />
+                        </Gallery>
+                    </div>
+                </section>
+                <h2 className="text-4xl mb-20">Usability Testing</h2>
+                <section className="mb-10 *:mb-6">
+                    <p>One of the final steps in the ESBoarding project was usability testing through a heuristic analysis specifically tailored for serious games. This evaluation, based on a set of heuristics developed by Fitchat and Jordan, played a critical role in verifying that the app met usability standards and fulfilled user needs, ensuring a positive onboarding experience for international IMX students.</p>
+                    <p>Key findings from the analysis included:</p>
+                    <ul className="list-disc pl-9">
+                        <li>ESBoarding’s mobile design allows students to conveniently access information anytime, supporting seamless navigation.</li>
+                        <li>Visual highlights and reminders maintain user engagement and motivation.</li>
+                        <li>Language terms are subtly integrated, helping students learn naturally without extra effort.</li>
+                        <li>Rewards like badges encourage progress, providing positive feedback.</li>
+                        <li>Clear indicators and progress bars enable students to track their achievements.</li>
+                        <li>The app provides in-task guidance and customizable avatars, fostering a personalized experience.</li>
+                    </ul>
+                    <p>Additionally, a formative evaluation was conducted with three current IMX students from the 6th and 8th semesters at ESB Business School in Reutlingen, who tested the prototype and provided feedback. The response was very positive, with students appreciating that "all information is in one place, and the structure is very clear."</p>
+                    <p>They valued the inclusion of basic vocabulary, saying it helped them prepare for studying in a foreign language. Suggestions for further development included adding more interactive language games and options for direct communication with peers.</p>
+                    <p>Overall, students were enthusiastic about the app's potential, viewing it as a valuable tool for easing the transition to Reutlingen and ESB Business School. One student summarized.</p>
                 </section>
             </main></div>
     )
